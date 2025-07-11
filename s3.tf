918b3f0f2e0661a144f784a63324c3b46e73d25c
Ashok <mails.ashok@yahoo.com>
2025-07-11 11:03:47 +0400

    Create s3.tf

---
+++ s3.tf
@@ -0,0 +1,41 @@
+provider "aws" {
+region = var.region
+}
+
+# terraform state file setup
+# create an S3 bucket for vpc to store the state file in
+
+resource "aws_s3_bucket" "terraform-state-storage-s3-ak" {
+   bucket = var.bucket_name
+ 
+    versioning {
+      enabled = true
+    }
+ 
+  #  lifecycle {
+   #   prevent_destroy = true
+  #  }
+ 
+    tags = {
+      Name = "S3 Bucket"
+    }      
+}
+
+
+
+# create a dynamodb table for locking the state file
+# resource "aws_dynamodb_table" "dynamodb-terraform-state-lock-devops4solutions" {
+#   name = "terraform-state-lock-dynamo-devops4solutions"
+#   hash_key = "LockID"
+#   read_capacity = 20
+#   write_capacity = 20
+ 
+#   attribute {
+#     name = "LockID"
+#     type = "S"
+#   }
+ 
+#   tags = {
+#     Name = "DynamoDB Terraform State Lock Table for devops4solutions"
+#   }
+# }
